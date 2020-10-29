a6ba45dee1c402f586e9d23afc46bf4918d6fa30
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-10-29 01:22:40 -0400

    Created update controller

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -3,8 +3,19 @@ const controller = {};
 controller.list = (req, res) => {
   req.getConnection((err, conn) => {
     conn.query("SELECT * FROM customer", (err, customers) => {
-      if (err) res.status(502).send("Error: " + err);
+      if (err) res.status(502).json({ menssage: err});
       res.json(customers);
+    });
+  });
+};
+
+controller.listById = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query("SELECT * FROM customer WHERE id = ?", [id], (err, customer) => {
+      if (err) res.status(502).send("Error: " + err);
+      if (customer.length == 0) res.status(404).json({ message: "User not found"});
+      else res.json(customer);
     });
   });
 };
@@ -14,23 +25,32 @@ controller.save = (req, res) => {
   req.getConnection((err, conn) => {
     conn.query("INSERT INTO customer set ?", [data], (err, customer) => {
       console.log(customer.insertId);
-      if (err) res.status(502).send("Error: " + err);
+      if (err) res.status(502).json({ menssage: err});
       if (customer.affectedRows > 0)
-        res.send(`Inserted id ${customer.insertId}`);
+        res.json({ message: `Inserted id ${customer.insertId}`});
     });
   });
 };
+
+controller.udpate = (req,res)=>{
+  const {id} = req.params;
+  const newCustomer = req.body;
+  req.getConnection((err, conn)=>{
+    conn.query("UPDATE customer SET ? WHERE id = ?", [newCustomer, id], (err, customer)=>{
+      if (err) res.status(502).send("Error: " + err);
+      if (customer.affectedRows > 0) res.json({ message: `ID:${id} was updated` });
+      else res.status(404).json({ message: `ID:${id} not found` });
+    })
+  })
+}
 
 controller.delete = (req, res) => {
   const { id } = req.params;
   req.getConnection((err, conn) => {
     conn.query("DELETE FROM customer WHERE id = ?", [id], (err, customer) => {
-      if (err) 
-        res.status(502).send("Error: " + err);
-      if(customer.affectedRows > 0) 
-        res.json(`ID:${id} was deleted`);
-      else
-        res.status(404).json(`ID:${id} not found`);
+      if (err) res.status(502).send("Error: " + err);
+      if (customer.affectedRows > 0) res.json({ message: `ID:${id} was deleted` });
+      else res.status(404).json({ message: `ID:${id} not found` });
     });
   });
 };
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -4,7 +4,9 @@ const customerController = require('../controllers/customerController');
 
 router
     .get('/', customerController.list)
+    .get('/:id', customerController.listById)
     .post('/add', customerController.save)
+    .post('/update/:id', customerController.udpate)
     .delete('/delete/:id', customerController.delete);
 
 module.exports = router;
